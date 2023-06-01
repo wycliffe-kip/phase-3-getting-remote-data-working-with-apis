1de097e17d9873e77eb104d500c64b41b5451602
Wycliffe <wycliffekiprono4@gmail.com>
2023-06-01 13:29:37 +0300

    done

--- nyc_api.rb
+++ nyc_api.rb
@@ -6,13 +6,13 @@ require 'net/http'
 
   URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
-  def get_programs
+  def get_response_body
     uri = URI.parse(URL)
     response = Net::HTTP.get_response(uri)
     response.body
   end
 
-  def program_school
+  def parse_json
     programs = JSON.parse(self.get_programs)
     programs.collect do |program|
       program["agency"]
@@ -21,5 +21,7 @@ require 'net/http'
 
 end
 
- programs = GetPrograms.new.get_programs
- puts programs
+#  programs = GetPrograms.new.get_programs
+#  puts programs
+programs = GetPrograms.new 
+puts programs.program_school.uniq
